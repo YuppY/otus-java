c4dc3e4f4a54e47e63a5c04e844c9556e6f9f75e
YuppY <ypetrov@gmail.com>
2020-03-24 14:34:55 +0500

    hw02 arraylist (#2)

---
+++ hw02-arraylist/src/main/java/ru/otus/hw/DIYArrayList.java
@@ -0,0 +1,223 @@
+package ru.otus.hw;
+
+import java.lang.System;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+public class DIYArrayList<T> implements List<T> {
+
+    private static final int INITIAL_CAPACITY = 128;
+
+    private Object[] array;
+    private int size = 0;
+
+    public DIYArrayList(int capacity) {
+        array = new Object[capacity];
+    }
+
+    public DIYArrayList(T... items) {
+        array = new Object[Math.max(INITIAL_CAPACITY, items.length)];
+        for (var item : items) {
+            array[size++] = item;
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        for (var i = 0; i < size; i++) {
+            if (array[i] == o) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Object[] toArray() {
+        var newArray = new Object[size];
+        System.arraycopy(array, 0, newArray, 0, size);
+        return newArray;
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean add(T t) {
+        if (size == array.length) {
+            var newArray = new Object[array.length * 2];
+            System.arraycopy(array, 0, newArray, 0, size);
+            array = newArray;
+        }
+
+        array[size++] = t;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T get(int index) {
+        if (index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (T) array[index];
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T set(int index, T element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        T oldElement = (T) array[index];
+        array[index] = element;
+        return oldElement;
+    }
+
+    @Override
+    public void add(int index, T element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return new DIYArrayListIterator();
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    private class DIYArrayListIterator implements ListIterator<T> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < size;
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return (T) array[index++];
+        }
+
+        @Override
+        public boolean hasPrevious() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public T previous() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int nextIndex() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public int previousIndex() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public void set(T e) {
+            if (index == 0) {
+                throw new IllegalStateException();
+            }
+            DIYArrayList.this.set(index - 1, e);
+        }
+
+        @Override
+        public void add(T e) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
